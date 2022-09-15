727766f8ee00edcf15fc9e5662cc30328b80b184
LiuTeiTei <ltt_catchyou@163.com>
2022-09-16 01:00:16 +0800

    doc: Life Cycle

--- life_cycle/src/main.rs
+++ life_cycle/src/main.rs
@@ -11,7 +11,7 @@
 } */
 
 // error[E0106]: missing lifetime specifier
-fn main() {
+/* fn main() {
     let string1 = String::from("abcd");
     let string2 = "xyz";
 
@@ -25,4 +25,41 @@ fn longest(x: &str, y: &str) -> &str {
     } else {
         y
     }
+} */
+
+/* struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+fn main() {
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
+} */
+
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+impl<'a> ImportantExcerpt<'a> {
+    fn level(&self) -> i32 {
+        3
+    }
+}
+
+impl<'a> ImportantExcerpt<'a> {
+    fn announce_and_return_part(&self, announcement: &str) -> &str {
+        println!("Attention please: {}", announcement);
+        self.part
+    }
+}
+
+fn main() {
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
 }
